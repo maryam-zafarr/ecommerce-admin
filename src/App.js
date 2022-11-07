44c490d4da122550134ed1d46e84774a05ee78e5
Maryam <maryam.zafar@devsinc.com>
2022-11-07 20:49:19 +0500

    Added missing authentication

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import Dashboard from "./pages/Dashboard";
 import Products from "./pages/Products";
 import NewProduct from "./pages/NewProduct";
 import Orders from "./pages/Orders";
+import Login from "./components/Auth/Login";
 
 const App = () => {
   return (
     <Switch>
+      <Route path="/login" exact>
+        <Login />
+      </Route>
       <Route path="/dashboard" exact>
         <Dashboard />
       </Route>
